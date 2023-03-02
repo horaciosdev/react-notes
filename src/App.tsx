4b1e747433cfd671c47c20e2fd19f12809f54dfd
Horacio <horacio.schumann@gmail.com>
2023-03-02 11:35:49 -0300

    feat: criação do formulário para adicionar nota

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Formulario from "./components/Formulario";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
 }
 
 export default App;
