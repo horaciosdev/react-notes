cd840110666b313c6640d0aceb1f9381ebe655c5
Horacio <horacio.schumann@gmail.com>
2023-03-04 12:14:24 -0300

    feat: realizados ajustes e estilização do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,17 @@ import { FaGithub, FaRocket } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center gap-2">
-      <button className="flex justify-center items-center gap-2">
-        <FaGithub /> GitHub
-        <a
-          href="https://github.com/horaciosdev/react-notes"
-          target="_blank"
-        ></a>
-      </button>{" "}
-      <button className="flex justify-center items-center gap-2">
-        <FaRocket /> Other Projects
-        <a href="https://my-portfolio-ten-navy.vercel.app/" target="_blank"></a>
-      </button>
+    <header className="bg-gray-800 p-1 flex justify-center items-center gap-2">
+      <a href="https://github.com/horaciosdev/react-notes" target="_blank">
+        <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
+          <FaGithub /> GitHub
+        </button>
+      </a>
+      <a href="https://my-portfolio-ten-navy.vercel.app/" target="_blank">
+        <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
+          <FaRocket /> Other Projects
+        </button>
+      </a>
     </header>
   );
 }
