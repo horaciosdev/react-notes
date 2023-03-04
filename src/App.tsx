35176f4978d722ae32b3a42d65922b1a341ad99f
Horacio <horacio.schumann@gmail.com>
2023-03-04 12:13:54 -0300

    feat: add header and footer components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import NotesApp from "./pages/NotesApp";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App flex flex-col min-h-screen bg-blue-100">
+      <Header />
       <NotesApp />
+      <Footer />
     </div>
   );
 }
