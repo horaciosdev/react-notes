3de9455dfa37f7f4b64f077f32a752a99c955253
Horacio <horacio.schumann@gmail.com>
2023-03-19 19:38:07 -0300

    update: newform and editform updated to MUI

--- src/components/NewNoteForm.tsx
+++ src/components/NewNoteForm.tsx
@@ -1,43 +1,81 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import CheckIcon from "@mui/icons-material/Check";
+import CancelIcon from "@mui/icons-material/Cancel";
+
 export default function NewNoteForm(props: any) {
   const { title, setTitle } = props;
   const { text, setText } = props;
 
   return (
-    <form className="flex flex-col max-w-xs w-96 shadow-md shadow-gray-800 justify-center item-center gap-4 bg-white mt-5 p-5 rounded-xl">
-      <h1 className="text-center text-xl font-bold">New Note</h1>
-      <label className="flex flex-col justify-center items-center">
-        Title:
-        <input
-          className="input-primary"
-          type="text"
+    <Box
+      sx={{
+        bgcolor: "background.paper",
+        display: "flex",
+        flexDirection: "column",
+        mt: 4,
+        gap: 2,
+        p: 3,
+        borderRadius: 2,
+        boxShadow: "0px 2px 5px 1px #222",
+        maxWidth: "300px",
+        width: "100%",
+      }}
+    >
+      <Typography variant="h5" sx={{ textAlign: "center", fontWeight: "500" }}>
+        New Note
+      </Typography>
+      <FormControl>
+        <InputLabel htmlFor="note-title">Title</InputLabel>
+        <OutlinedInput
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          id="note-title"
+          placeholder="Choose a title"
+          label="Name"
         />
-      </label>
-      <label className="flex flex-col justify-center items-center">
-        Text:
-        <textarea
-          className="textarea-primary h-40"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      </label>
-      <div className="flex justify-center items-center gap-3">
-        <button
-          className="btn-success w-40"
-          type="submit"
+      </FormControl>
+      <TextField
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        label="Note"
+        multiline
+        rows={6}
+        placeholder="Write your note"
+      />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 1,
+        }}
+      >
+        <Button
           onClick={() => props.addNewNote()}
+          variant="outlined"
+          color="success"
+          startIcon={<CheckIcon />}
         >
           Add Note
-        </button>
-
-        <button
-          className="btn-danger w-40"
+        </Button>
+        <Button
           onClick={() => props.handleCancelNote()}
+          variant="outlined"
+          color="error"
+          endIcon={<CancelIcon />}
         >
           Cancel
-        </button>
-      </div>
-    </form>
+        </Button>
+      </Box>
+    </Box>
   );
 }
