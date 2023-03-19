7bd042839d1c8159b89ef3342a1005a6a7c1dee9
Horacio <horacio.schumann@gmail.com>
2023-03-18 23:40:08 -0300

    icons changed to material-icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
-import { FaGithub, FaRocket } from "react-icons/fa";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 
 export default function Header() {
   return (
     <header className="bg-gray-800 p-1 flex justify-center items-center gap-2">
       <a href="https://github.com/horaciosdev/react-notes" target="_blank">
         <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
-          <FaGithub /> GitHub
+          <GitHubIcon /> GitHub
         </button>
       </a>
       <a href="https://my-portfolio-ten-navy.vercel.app/" target="_blank">
         <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
-          <FaRocket /> Other Projects
+          <RocketLaunchIcon /> Other Projects
         </button>
       </a>
     </header>
