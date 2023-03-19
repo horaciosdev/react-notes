f9129ac037b5f919438461be16402fde9a291328
Horacio <horacio.schumann@gmail.com>
2023-03-18 23:39:47 -0300

    useState and counter removed from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NotesApp from "./pages/NotesApp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App flex flex-col min-h-screen bg-blue-100">
       <Header />
