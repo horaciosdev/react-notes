c7cb45e5412cfbf5cc7deb0f5da806d9d005a5d7
Horacio <horacio.schumann@gmail.com>
2023-03-19 23:33:33 -0300

    update: created themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,70 @@
+import { useState } from "react";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NotesApp from "./pages/NotesApp";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import { Box } from "@mui/system";
+
 function App() {
+  const [lightMode, setLightMode] = useState(false);
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#2196f3",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+      background: {
+        default: "#ffffff",
+        paper: "#f5f5f5",
+      },
+      text: {
+        primary: "#000000",
+        secondary: "#757575",
+        disabled: "#bdbdbd",
+      },
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#2196f3",
+      },
+      secondary: {
+        main: "#f50057",
+      },
+      background: {
+        default: "#121212",
+        paper: "#1e1e1e",
+      },
+      text: {
+        primary: "#ffffff",
+        secondary: "#b0bec5",
+        disabled: "#757575",
+      },
+    },
+  });
+
   return (
-    <div className="App flex flex-col min-h-screen bg-blue-100">
-      <Header />
-      <NotesApp />
-      <Footer />
-    </div>
+    <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
+      <Box
+        bgcolor={"background.default"}
+        color={"text.primary"}
+        sx={{ display: "flex", flexDirection: "column", width: "100%" }}
+      >
+        <Header lightMode={lightMode} setLightMode={setLightMode} />
+        <NotesApp />
+        <Footer />
+      </Box>
+    </ThemeProvider>
   );
 }
 
