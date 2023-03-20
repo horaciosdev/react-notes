83f1dee63758587b7eddde0901388b2300bd07c1
Horacio <horacio.schumann@gmail.com>
2023-03-19 23:34:47 -0300

    update: change header to MUI and add a switch theme button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,60 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { Box, Button, Link, Switch } from "@mui/material";
 
-export default function Header() {
+export default function Header(props: any) {
+  const { lightMode, setLightMode } = props;
   return (
-    <header className="bg-gray-800 p-1 flex justify-center items-center gap-2">
-      <a href="https://github.com/horaciosdev/react-notes" target="_blank">
-        <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
-          <GitHubIcon /> GitHub
-        </button>
-      </a>
-      <a href="https://my-portfolio-ten-navy.vercel.app/" target="_blank">
-        <button className="bg-gray-800 text-gray-300 flex justify-center items-center gap-1 border border-gray-400 rounded-md pl-2 pr-2 hover:invert">
-          <RocketLaunchIcon /> Other Projects
-        </button>
-      </a>
-    </header>
+    <Box
+      sx={{
+        bgcolor: "background.paper",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        p: "2px",
+        gap: 1,
+      }}
+    >
+      <Box
+        sx={{
+          margin: "0 auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 1,
+        }}
+      >
+        <Button variant="outlined" startIcon={<GitHubIcon />} size="small">
+          <Link
+            href="https://github.com/horaciosdev/react-notes"
+            target="_blank"
+            underline="none"
+          >
+            GitHub
+          </Link>
+        </Button>
+        <Button
+          variant="outlined"
+          startIcon={<RocketLaunchIcon />}
+          size="small"
+        >
+          <Link
+            href="https://my-portfolio-ten-navy.vercel.app/"
+            target="_blank"
+            underline="none"
+          >
+            Other Projects
+          </Link>
+        </Button>
+      </Box>
+
+      <Box>
+        <DarkModeIcon />
+        <Switch onChange={(e) => setLightMode(lightMode ? false : true)} />
+        <LightModeIcon />
+      </Box>
+    </Box>
   );
 }
