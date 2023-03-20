b7ce55336cbf424cfa24d2610923f97c4674426d
Horacio <horacio.schumann@gmail.com>
2023-03-19 23:34:18 -0300

    update: change footer to MUI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,55 @@
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { Box, IconButton, Link, Typography } from "@mui/material";
 
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   const developerName = "Horacio Schumann";
 
   return (
-    <footer className="mt-auto bg-gray-800 text-gray-300 p-4 flex flex-col justify-center items-center">
-      <p>
+    <Box
+      sx={{
+        textAlign: "center",
+        p: 2,
+      }}
+    >
+      <Typography>
         {developerName} - {currentYear}
-      </p>
-      <p>
+      </Typography>
+      <Typography>
         Contato:{" "}
-        <a href="mailto:horacio.schumann@gmail.com">
+        <Link
+          href="mailto:horacio.schumann@gmail.com"
+          color="inherit"
+          underline="hover"
+        >
           horacio.schumann@gmail.com
-        </a>
-      </p>
-      <div className="flex gap-2 text-3xl">
-        <a href="https://www.linkedin.com/in/horaciosdev" target="_blank">
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/horaciosdev" target="_blank">
-          <FaGithub />
-        </a>
-      </div>
-      <p>
+        </Link>
+      </Typography>
+      <Box sx={{ textAlign: "center" }}>
+        <Link
+          href="https://www.linkedin.com/in/horaciosdev"
+          target="_blank"
+          color="inherit"
+        >
+          <IconButton aria-label="delete" size="large" color="inherit">
+            <LinkedInIcon />
+          </IconButton>
+        </Link>
+        <Link
+          href="https://github.com/horaciosdev"
+          target="_blank"
+          underline="hover"
+          color="inherit"
+        >
+          <IconButton aria-label="delete" size="large" color="inherit">
+            <GitHubIcon />
+          </IconButton>
+        </Link>
+      </Box>
+      <Typography>
         Todos os direitos reservados © {currentYear} {developerName}
-      </p>
-    </footer>
+      </Typography>
+    </Box>
   );
 }
